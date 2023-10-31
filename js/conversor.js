32b373731ebdb8f44ecbacf3c0919a793fd71a99
gerardo montini <germontini@hotmail.com>
2023-10-31 16:46:00 -0300

    primer commit

---
+++ js/conversor.js
@@ -0,0 +1,22 @@
+function convertir(){
+    let valore = parseInt(document.getElementById("valor").value);
+    let resultado = 0;
+    let dolar = 350;
+    let euro = 400;
+    let bitcoin = 10500;
+    if(document.getElementById("uno").checked){
+        resultado = valore / dolar;
+        alert("El cambio de Pesos a Dolar es US$"  + resultado.toFixed(2))
+    }
+    else if (document.getElementById("dos").checked){
+        resultado = valore / euro;
+        alert("El cambio de Pesos a Euros es: Є"  + resultado.toFixed(2));
+    }
+    else if (document.getElementById("tres").checked){
+        resultado = valore / euro;
+        alert("El cambio de Pesos a Bitcoins es: ₿"  + resultado.toFixed(2));
+    }
+    else{
+        alert("Tienes que completar todos los campos");
+    }
+}
