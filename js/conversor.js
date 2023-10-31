4eb75903416624c9738d73562f5abd4f5baab1d3
gerardo montini <germontini@hotmail.com>
2023-10-31 18:33:39 -0300

    mejore conversor

--- js/conversor.js
+++ js/conversor.js
@@ -5,23 +5,29 @@ function convertir(){
     let euro = 400;
     let bitcoin = 10500;
     let real = 0.14;
-    if(document.getElementById("uno").checked){
-        resultado = valore / dolar;
-        alert("El cambio de Pesos a Dolar es US$"  + resultado.toFixed(2))
-    }
-    else if (document.getElementById("dos").checked){
-        resultado = valore / euro;
-        alert("El cambio de Pesos a Euros es: Є"  + resultado.toFixed(2));
-    }
-    else if (document.getElementById("tres").checked){
-        resultado = valore / euro;
-        alert("El cambio de Pesos a Bitcoins es: ₿"  + resultado.toFixed(2));
-    }
-    else if (document.getElementById("cuatro").checked){
-        resultado = valore / euro;
-        alert("El cambio de Pesos a Reales es: R$"  + resultado.toFixed(2));
-    }
-    else{
-        alert("Tienes que completar todos los campos");
+    const resultadoElement = document.getElementById("resultado");
+    if (isNaN(valore)) {
+        resultadoElement.textContent = "Debes ingresar una cantidad válida en Pesos (COP).";
+    } else {
+        if(document.getElementById("uno").checked){
+            resultado = valore / dolar;
+            resultadoElement.textContent = "El cambio de Pesos a Dólares es US$" + resultado.toFixed(2);
+        }
+        else if (document.getElementById("dos").checked){
+            resultado = valore / euro;
+            resultadoElement.textContent = "El cambio de Pesos a Euros es: Є" + resultado.toFixed(2);
+        }
+        else if (document.getElementById("tres").checked){
+            resultado = valore / bitcoin;
+            resultadoElement.textContent = "El cambio de Pesos a Bitcoins es: ₿" + resultado.toFixed(6); // Mostrar 6 decimales
+        }
+        else if (document.getElementById("cuatro").checked){
+            resultado = valore / real;
+            resultadoElement.textContent = "El cambio de Pesos a Reales es: R$" + resultado.toFixed(2);
+        }
+        else {
+            resultadoElement.textContent = "Debes seleccionar una moneda.";
+        }
     }
 }
+
