2b109f8aca2806b7c5f565860e7bab7516d400bf
gerardo montini <germontini@hotmail.com>
2023-11-01 18:44:51 -0300

    separe nombre y apellido

--- js/conversor.js
+++ js/conversor.js
@@ -1,24 +1,19 @@
 
-// Solicitar el nombre del usuario
 let nombre = prompt("Por favor, ingresa tu nombre:");
 let apellido = prompt("Por favor, ingresa tu apellido:");
+let edad = prompt("Hola " + nombre + " " + apellido + " , ingresa tu edad:");
 
-// Solicitar la edad del usuario
-let edad = prompt("Hola " + nombre + ", ingresa tu edad:");
-
-// Convertir la entrada de edad a un número
 edad = parseInt(edad);
 
-// Verificar si el usuario es mayor de 18 años
 if (!isNaN(edad) && edad >= 18) {
     alert("Bienvenido, " + nombre + ". Eres mayor de 18 años y puedes entrar.");
 } else {
-    alert("Lo siento, " + nombre + ". Eres menor de 18 años y no puedes entrar.");
+    alert(
+        "Lo siento, " + nombre + ". Eres menor de 18 años y no puedes entrar."
+    );
 }
 
-
-
-function convertir(){
+function convertir() {
     let valore = parseInt(document.getElementById("valor").value);
     let resultado = 0;
     let dolar = 350;
@@ -27,30 +22,30 @@ function convertir(){
     let real = 0.14;
     const resultadoElement = document.getElementById("resultado");
     if (isNaN(valore)) {
-        resultadoElement.textContent = "Debes ingresar una cantidad válida en Pesos (COP).";
+        resultadoElement.textContent =
+            "Debes ingresar una cantidad válida en Pesos (COP).";
     } else {
-        if(document.getElementById("uno").checked){
+        if (document.getElementById("uno").checked) {
             resultado = valore / dolar;
-            resultadoElement.textContent = "El cambio de Pesos a Dólares es US$" + resultado.toFixed(2);
-        }
-        else if (document.getElementById("dos").checked){
+            resultadoElement.textContent =
+                "El cambio de Pesos a Dólares es US$" + resultado.toFixed(2);
+        } else if (document.getElementById("dos").checked) {
             resultado = valore / euro;
-            resultadoElement.textContent = "El cambio de Pesos a Euros es: Є" + resultado.toFixed(2);
-        }
-        else if (document.getElementById("tres").checked){
+            resultadoElement.textContent =
+                "El cambio de Pesos a Euros es: Є" + resultado.toFixed(2);
+        } else if (document.getElementById("tres").checked) {
             resultado = valore / bitcoin;
-            resultadoElement.textContent = "El cambio de Pesos a Bitcoins es: ₿" + resultado.toFixed(2); 
-        }
-        else if (document.getElementById("cuatro").checked){
+            resultadoElement.textContent =
+                "El cambio de Pesos a Bitcoins es: ₿" + resultado.toFixed(2);
+        } else if (document.getElementById("cuatro").checked) {
             resultado = valore / real;
-            resultadoElement.textContent = "El cambio de Pesos a Reales es: R$" + resultado.toFixed(2);
-        }
-        else {
+            resultadoElement.textContent =
+                "El cambio de Pesos a Reales es: R$" + resultado.toFixed(2);
+        } else {
             resultadoElement.textContent = "Debes seleccionar una moneda.";
         }
     }
 }
-
 
 //Crear un algoritmo utilizando un ciclo.
 
@@ -61,5 +56,3 @@ while (contador <= 10) {
 
     contador++;
 }
-
-
